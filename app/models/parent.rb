36382c89f56184f68a2e9461801956836dd5c43a
Maria <mliberov@gmail.com>
2015-02-06 17:04:26 -0500

    Add has_secure_password to parent and student

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,3 +1,4 @@
 class Parent < ActiveRecord::Base
+  has_secure_password
   belongs_to :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ActiveRecord::Base
+  has_secure_password
   has_many :grades
   has_many :parents
   belongs_to :teacher
