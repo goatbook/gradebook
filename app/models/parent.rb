0466c4dffcecec3584060c52470cab4d58f7f434
Topstitch <kallen@spoonflower.com>
2015-02-06 15:32:15 -0500

    Has many belongs to

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,2 +1,3 @@
 class Parent < ActiveRecord::Base
+  belongs_to :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+  has_many :grades
+  has_many :parents
+  belongs_to :teacher
 end
