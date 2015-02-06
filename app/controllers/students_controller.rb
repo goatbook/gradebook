e61a16a3a41c244a5991fe0740dd3612b61ea132
Maria <mliberov@gmail.com>
2015-02-06 14:21:19 -0500

    Edit controllers and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,62 @@
 class StudentsController < ApplicationController
+  #before_action :check_logged_in
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
+
   def index
+    @students = Student.all
   end
 
   def new
+    @student = Student.new
   end
 
   def edit
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "Welcome to your page!"
+      redirect_to students_path, notice: 'Student was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
+    if @student.update(student_params)
+      redirect_to students_path, notice: 'Student was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @student.destroy
+    redirect_to students_url, notice: 'Student was successfully destroyed.'
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password_digest, :teacher_id )
+  end
+
+  def make_session(student)
+    session[:student_id] = student.id
+    session[:name] = "Jane Doe"
+  end
+
+
+  def check_logged_in
+    if session[:student_id].blank?
+      redirect_to student_path
+    end
   end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,22 +1,62 @@
 class TeachersController < ApplicationController
+  #before_action :check_logged_in
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+
   def index
+    @teachers = Teacher.all
   end
 
   def new
+    @teacher = Teacher.new
   end
 
   def edit
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      flash[:success] = "Welcome to your page!"
+      redirect_to teachers_path, notice: 'Teacher was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
+    if @teacher.update(teacher_params)
+      redirect_to teachers_path, notice: 'Teacher was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @teacher.destroy
+    redirect_to teachers_url, notice: 'Teacher was successfully destroyed.'
+  end
+
+  private
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :email, :password_digest )
+  end
+
+  def make_session(teacher)
+    session[:teacher_id] = teacher.id
+    session[:name] = "Jane Doe"
+  end
+
+
+  def check_logged_in
+    if session[:teacher_id].blank?
+      redirect_to teacher_path
+    end
   end
 end
