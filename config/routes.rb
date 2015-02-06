a5a19c8682c70e864fab4ef9d5fd5b7ef2e12440
Maria <mliberov@gmail.com>
2015-02-06 18:42:24 -0500

    Add splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'teachers_login/login'
   post 'teachers_login/login'
 
@@ -21,7 +23,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'students#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
