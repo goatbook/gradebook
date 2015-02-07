6859e893b9dc0582e8f2e77d9b07a69ff4cf06d2
Topstitch <kallen@spoonflower.com>
2015-02-07 15:50:35 -0500

    Change password button appearsfor all users

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,2 +1,16 @@
 module GradesHelper
+
+  # def display_all_scores(items)
+  #   result = "<table>"
+  #   items.each do |i|
+  #     result << "<tr>"
+  #     result << "<td>#{i.date}</td>"
+  #     result << "<td>#{i.description}</td>"
+  #     result << "<td>#{i.score}</td>"
+  #     result << "</tr>"
+  #   end
+  #   result << "</table>"
+  # end
+  # result.html_safe
+
 end
