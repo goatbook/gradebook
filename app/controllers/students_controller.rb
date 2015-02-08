a35ac6d8f1f4577087f07ff3d6d1d760843ecdcf
Maria <mliberov@gmail.com>
2015-02-08 11:14:28 -0500

    Add grade edit link

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -46,7 +46,7 @@ class StudentsController < ApplicationController
   end
 
   def student_params
-    params.require(:student).permit(:name, :email, :password, :teacher_id )
+    params.require(:student).permit(:name, :email, :password, :teacher_id)
   end
 
   def make_session(student)
--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -3,24 +3,25 @@ module GradesHelper
   def student_grades(student)
     grades = student.grades
     result = "<table>"
-    if session[:teacher_id]
+    if session[:teacher_id] && grades
       grades.each do |g|
         result << "<tr>"
-        result << "<td>#{g}</td>"
+        result << "<td>#{g.description}</td>"
+        result << "<td>" + link_to("Edit Grade", edit_grade_path(g.id)) + "</td>"
         result << "</tr>"
       end
       result << "</table>"
     elsif session[:student_id]
       grades.each do |g|
         result << "<tr>"
-        result << "<td>#{g}</td>"
+        result << "<td>#{g.description}</td>"
         result << "</tr>"
       end
       result << "</table>"
     elsif session[:parent_id]
       grades.each do |g|
         result << "<tr>"
-        result << "<td>#{g}</td>"
+        result << "<td>#{g.description}</td>"
         result << "</tr>"
       end
     end
