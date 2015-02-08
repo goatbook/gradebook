f9fb194914bc247b51eb25d57f5b060da4d67f31
Maria <mliberov@gmail.com>
2015-02-08 17:20:08 -0500

    Edit grades helper

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -3,23 +3,7 @@ module GradesHelper
   def student_grades(student)
     grades = student.grades
     result = "<table>"
-    if session[:teacher_id] && grades
-      grades.each do |g|
-        result << "<tr>"
-        # result << "<td>#{g.description}</td>"
-        # result << "<td>#{g.score}</td>"
-        result << "</tr>"
-      end
-      result << "</table>"
-    elsif session[:student_id] && grades
-      grades.each do |g|
-        result << "<tr>"
-        result << "<td>#{g.description}</td>"
-        result << "<td>#{g.score}</td>"
-        result << "</tr>"
-      end
-      result << "</table>"
-    elsif session[:parent_id] && grades
+    if grades
       grades.each do |g|
         result << "<tr>"
         result << "<td>#{g.description}</td>"
