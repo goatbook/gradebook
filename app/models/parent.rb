0dca1efe53afb46ec8d07c80a1c6c56e1eaa7e99
Maria <mliberov@gmail.com>
2015-02-07 23:40:24 -0500

    Fix buttons and add validates statements #shipit

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,4 +1,6 @@
 class Parent < ActiveRecord::Base
   has_secure_password
   belongs_to :student
+
+  validates :email, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,7 @@ class Student < ActiveRecord::Base
   has_many :parents
   belongs_to :teacher
 
+  validates :email, presence: true
 
   def self.all_students
     return self.all
