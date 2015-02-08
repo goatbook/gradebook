cdd86c7d91aa2c0c9c9ab3e9d520b97630d15e1c
Maria <mliberov@gmail.com>
2015-02-07 22:06:01 -0500

    Add grades helper and grades functionality #shipit

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,2 +1,30 @@
 module GradesHelper
+
+  def student_grades(student)
+    grades = student.grades
+    result = "<table>"
+    if session[:teacher_id]
+      grades.each do |g|
+        result << "<tr>"
+        result << "<td>#{g}</td>"
+        result << "</tr>"
+      end
+      result << "</table>"
+    elsif session[:student_id]
+      grades.each do |g|
+        result << "<tr>"
+        result << "<td>#{g}</td>"
+        result << "</tr>"
+      end
+      result << "</table>"
+    elsif session[:parent_id]
+      grades.each do |g|
+        result << "<tr>"
+        result << "<td>#{g}</td>"
+        result << "</tr>"
+      end
+    end
+    result << "</table>"
+    result.html_safe
+  end
 end
